015a21ca888ee507b49d6212e853d27cd59403e9
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-18 13:30:15 +0000

    Final commit - create logic to lift the ball up by interacting with keyboard space.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-//dribble the ball on the platform using any key on a keyboard. When a user presses a key down, it should lift the ball up. When the user releases the key, the ball should drop.
+//dribble the ball on the platform using any key on a keyboard. When a user presses a key down (space), it should lift the ball up. When the user releases the key, the ball should drop.
+
+const ball = document.getElementById("float-circle");
+
+function up(e) {
+  // console.log(typeof e.keyCode);
+  if (e.keyCode === 32) {
+    ball.style.bottom = "250px";
+  }
+}
+
+function down(e) {
+  if (e.keyCode === 32) {
+    ball.style.bottom = "50px";
+  }
+}
+
+document.addEventListener("keydown", up);
+document.addEventListener("keyup", down);
